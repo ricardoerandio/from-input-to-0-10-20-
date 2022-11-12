b96f29467942199c2414535cd7c8ed7bbc954d4e
ryuic <ryuic@DESKTOP-FTRABAR>
2022-11-12 13:24:04 +0800

    Add project files.

---
+++ from 0 to input(10-20)/from 0 to input(10-20).cpp
@@ -0,0 +1,36 @@
+// from 0 to input(10-20).cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 20;
+    cout << "enter number:";
+        cin >> a;
+        if (a>20)
+        {
+            cout << "entered number is greater than 20 try again";
+        }
+        else if (a<10)
+        {
+            cout << "entered number is less than 10 try again";
+        }
+        else while (a>=0)
+        {
+            cout << a << "\n";
+            a--;
+        }
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
